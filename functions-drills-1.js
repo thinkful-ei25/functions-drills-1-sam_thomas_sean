c76e77adc24607c158adba9d468a63ce194b37e6
Sean Nealon <signalflowsean@gmail.com>
2018-09-24 15:35:23 -0400

    error checking

--- functions-drills-1.js
+++ functions-drills-1.js
@@ -1,21 +1,45 @@
 'use strict'; 
 
 function createGreeting(name, age) {
-  const yearOfBirth = getYearOfBirth(age);
+  let yearOfBirth;
+  try {
+    checkNameEntered(name);
+  } catch(e){ 
+    console.error(e.message); 
+  }
+  try { 
+    checkAgeEntered(age); 
+    yearOfBirth = getYearOfBirth(age);
+  } catch(err){ 
+    console.error(err.message); 
+  }
+
   return `Hi, my name is ${name} and I\'m ${age} years old. 
   So inevitably I was born in ${yearOfBirth} `;
 }
 
-function getYearOfBirth(age){
-  if (age < 0) {
-    throw new Error('Age can not be negative');
+function checkNameEntered(name){ 
+  if (typeof name === 'undefined'){ 
+    throw new Error('Name is not defined'); 
   }
-  return 2018 - age;
 }
 
-try {
-const greeting1 = createGreeting('Thomas', -1);
-console.log(greeting1);
-} catch(e) {
-  console.error(e.message);
+function checkAgeEntered(age){ 
+  if (typeof age === 'undefined'){ 
+    throw new Error('Age is not defined'); 
+  }
 }
+
+function getYearOfBirth(age){
+  if (age < 0) { 
+    throw new Error('Age can not be negative');
+  } 
+  return 2018 - age;
+  
+}
+
+let name = 'john'; 
+let age = 20; 
+
+const greeting1 = createGreeting(name, age);
+console.log(greeting1);
