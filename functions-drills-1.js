f03c090008c342aad8387658c30442d644e46291
Thomas Chun <tchun2055@gmail.com>
2018-09-24 11:51:08 -0700

    hardcoded string with parameters

--- functions-drills-1.js
+++ functions-drills-1.js
@@ -1,9 +1,9 @@
 'use strict'; 
 
-function createGreeting() {
-  return 'Hi, my name is Thomas and I\'m 30 years old';
+function createGreeting(name, age) {
+  return `Hi, my name is ${name} and I\'m ${age} years old`;
 }
 
 
-const greeting1 = createGreeting();
+const greeting1 = createGreeting("Thomas", 30);
 console.log(greeting1);
