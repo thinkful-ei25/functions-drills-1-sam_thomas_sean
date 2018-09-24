212e47914be11d176ced4aac06d5f9b6cd568379
Sean Nealon <signalflowsean@gmail.com>
2018-09-24 14:54:31 -0400

    Year of brith statement added

--- functions-drills-1.js
+++ functions-drills-1.js
@@ -1,9 +1,10 @@
 'use strict'; 
 
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I\'m ${age} years old`;
+  const yearOfBirth = 2018 - age; 
+  return `Hi, my name is ${name} and I\'m ${age} years old. \n So inevitably I was born in ${yearOfBirth} `;
 }
 
 
-const greeting1 = createGreeting("Thomas", 30);
+const greeting1 = createGreeting('Thomas', 30);
 console.log(greeting1);
