4814698e1de315b40a3fa3da6490f9e8663afae5
Sam Dalton <spd4nf@virginia.edu>
2018-09-24 13:58:42 -0500

    added getYearOfBirth function

--- functions-drills-1.js
+++ functions-drills-1.js
@@ -1,10 +1,14 @@
 'use strict'; 
 
 function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age; 
-  return `Hi, my name is ${name} and I\'m ${age} years old. \n So inevitably I was born in ${yearOfBirth} `;
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I\'m ${age} years old. 
+  So inevitably I was born in ${yearOfBirth} `;
 }
 
+function getYearOfBirth(age){
+    return 2018 - age;
+}
 
 const greeting1 = createGreeting('Thomas', 30);
 console.log(greeting1);
