902e2077dd683593d5c06e76c5f0a23c1d4db90c
Sean Nealon <signalflowsean@gmail.com>
2018-09-24 14:40:10 -0400

    wrap statement in a function

--- functions-drills-1.js
+++ functions-drills-1.js
@@ -1,1 +1,7 @@
-console.log('Hi, my name is Thomas and I\'m 30 years old');
+'use strict'; 
+
+function createGreeting() {
+  console.log('Hi, my name is Thomas and I\'m 30 years old');
+}
+
+createGreeting(); 
